f258a903103c2f0a0ae62c41c3b100facc52966a
Tandoh004 <atandoh004@st.ug.edu.gh>
2023-06-11 22:22:01 +0000

    Flask removed

--- Python_Basics.py
+++ Python_Basics.py
@@ -1,5 +1,4 @@
-import flask
-#Print Hello world
+# Print Hello world
 print("Hellow world")
 
 # Taking input from a user
@@ -10,46 +9,42 @@ age = input("How old are you?: ")
 birth_year = 2023 - int(age)
 print(birth_year)
 
-
-costPrice = input("How much is the can of millo? ")
+costPrice = input("How much is the can of Millo? ")
 payment = input("your payment: ")
-sub = int(costPrice) -int(payment)
+sub = int(costPrice) - int(payment)
 print("Balance: " + str(sub))
 
-myprog = " I love programming and i will love fast"
-print(myprog.upper())
-print(myprog.lower())
+my_prog = " I love programming and i will love fast"
+print(my_prog.upper())
+print(my_prog.lower())
 
-#Car speed check
+# Car speed check
 carSpeed = 80
 if carSpeed > 120:
-    print("Driver you drive too fasr")
+    print("Driver you drive too fast")
 elif carSpeed < 50:
     print("Driver! you are driving too slow, this is a highway")
 else:
     print("Driver your speed is good")
 print("Ready")
 
-
-#Checking Qualification of candidate for employment
+# Checking Qualification of candidate for employment
 degree = input("What is your highest degree? Bachelor, Master or PhD")
 experience = input("How many years of experience do you have?")
 if degree == "Master" or degree == "master" or degree == "PhD" or degree == "phd":
-    if int(experience)>= 2:
+    if int(experience) >= 2:
         print("Congratulation! You are accepted for the interview")
     else:
         print("Sorry You don't have enough experience")
 else:
     print("Sorry, You don't have the require degree for this job, Better luck next time")
 
-
 ## While loop
 
 i = 0
-while i<=5:
+while i <= 5:
     print(i)
-    i =i+1
-
+    i = i + 1
 
 # for loop
 temperature = [67, 30, 70, 91]
@@ -60,11 +55,9 @@ numbers = range(10)
 for number in numbers:
     print(number)
 
-
-
-#You are a university professor that gave a practice test to its students.
-#Your job is to grade your students based on their percentage grade.
-#Grading scale :90–100% = A  80–89% = B  70–79% = C  60–69% = D  0–59% = F
+# You are a university professor that gave a practice test to its students.
+# Your job is to grade your students based on their percentage grade.
+# Grading scale :90–100% = A  80–89% = B  70–79% = C  60–69% = D  0–59% = F
 
 percentage_grade = 75
 
@@ -72,9 +65,9 @@ if percentage_grade >= 90:
     print("A")
 elif percentage_grade >= 80:
     print("B")
-elif percentage_grade >= 70-79:
+elif percentage_grade >= 70 - 79:
     print("C")
-elif percentage_grade >= 60-69:
+elif percentage_grade >= 60 - 69:
     print("D")
 else:
     print("F")
